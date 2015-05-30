9d864a2e92b4276c7cb448057fa1dab98ec85ebc
shirleychou1 <shirleychou1@gmail.com>
2015-05-30 14:33:14 +0800

    update

--- mysite/urls.py
+++ mysite/urls.py
@@ -10,6 +10,6 @@ urlpatterns = patterns('blog.views',
 
 urlpatterns += patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^blog/', include('blog.urls', namespace='blog')),
+    # url(r'^blog/', include('blog.urls', namespace='blog')),
 )
 
